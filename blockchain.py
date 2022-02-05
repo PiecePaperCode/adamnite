d0932fd53861de4cf87290e77df47694aabd05fa
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-02-05 16:34:18 -0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 import time
-import json,requests
-import blockchainutils
-from random import choice,randint
-
+from blockchainutils import hashing
+import json, requests
+from random import randint, choice
+#Implementation based off basic POS repository by eltnieg
 
 DATE = datetime.now()
 GENESIS_BLOCK = {
@@ -11,26 +11,51 @@ GENESIS_BLOCK = {
     "Timestamp": str(DATE),
     "BPM": 0, #instead of transactions
     "PrevHash": "",
-    "Witness": "" #address that proposed block
+    "Validator": "" #address to receive the reward {validator, weight, age}
 }
+GENESIS_BLOCK2 = {
+    "Index": 0,
+    "Timestamp": str(DATE),
+    "BPM": 0, #instead of transactions
+    "PrevHash": "",
+    "Validator": "" #address to receive the reward {validator, weight, age}
+}
+GENESIS_BLOCK3 = {
+    "Index": 0,
+    "Timestamp": str(DATE),
+    "BPM": 0, #instead of transactions
+    "PrevHash": "",
+    "Validator": "" #address to receive the reward {validator, weight, age}
+}
+
+GENESIS_BLOCK4 = {
+    "Index": 0,
+    "Timestamp": str(DATE),
+    "BPM": 0, #instead of transactions
+    "PrevHash": "",
+    "Validator": "" #address to receive the reward {validator, weight, age}
+}
+
 
 class Blockchain(object):
 
 
     def __init__(self, _genesisBlock, account):
-
+        """
+            If the genesis block is valid, create chain
+        """
         self.blockChain = []
         self.tempBlocks = []
         #self.candidateBlocks = [] #constains block
         self.myCurrBlock = {}
         #self.announcements = []
-        self.validators = set() # stakers and balance
-        self.witnesses = set()
+        self.validators = set()
+        self.pool = []
         #self.unconfirmed_txns = []
         self.nodes = set()
-        self.myAccount = {'Address': '', 'Stake': 0, 'Age': 0, 'Votes For': 0, 'Voted for': ''}
+        self.myAccount = {'Address': '', 'Weight': 0, 'Age': 0, 'Votes For': 0}
         self.myAccount['Address'] = account['Address']
-        self.myAccount['Stake'] = account['Stake']
+        self.myAccount['Weight'] = account['Weight']
         try:
             genesisBlock = self.generate_genesis_block(_genesisBlock)
             if self.is_block_valid(genesisBlock):
@@ -39,6 +64,24 @@ class Blockchain(object):
                 raise Exception('Unable to verify block')
         except Exception as e:
             print('Invalid genesis block.\nOR\n' + str(e))
+
+
+
+    def voting(self, account):
+        voted_to = []
+        if account['Address'] in self.validators:
+            voted_to = account['Address']
+        else:
+            voted_to = choice(self.validators)
+        voted_to['Votes For'] += account['Weight']
+
+    def set_pool(self):
+        sort(self.validators,self.validators['Votes For'])
+        for validator in self.validators:
+            if self.validators.index(validator)/len(self.validators) > 0.10:
+                self.pool.append(validator)
+
+
 
     def is_block_valid(self, block, prevBlock={}):
         try:
@@ -66,26 +109,24 @@ class Blockchain(object):
         block['Hash'] = _hash
         return True
 
-    def give_vote(self, voted_for, ):
-
-
-    def generate_new_block(self, bpm=randint(1,1000), oldBlock='', address=''):
-        if self.myCurrBlock:
-            return self.myCurrBlock
-        prevHash = self.blockChain[-1]['Hash']
-        index = len(self.blockChain) if not oldBlock else oldBlock['Index'] + 1
-        address =  self.get_validator(self.myAccount) if not address else address
-        newBlock = {
-            "Index": index,
-            "Timestamp": str(datetime.now()),
-            "BPM": bpm, #instead of transactions
-            "PrevHash": prevHash,
-            "Witness": address
-        }
-        newBlock["Hash"] = self.hashing(newBlock)
-        assert self.is_block_valid(newBlock)
-        self.myCurrBlock = newBlock
-        return newBlock
+    def generate_new_block(self, bpm=randint(53, 63), oldBlock='', address=''):
+        if self.myAccount['Address'] in self.pool:
+            if self.myCurrBlock:
+                return self.myCurrBlock
+            prevHash = self.blockChain[-1]['Hash']
+            index = len(self.blockChain) if not oldBlock else oldBlock['Index'] + 1
+            address =  self.get_validator(self.myAccount) if not address else address
+            newBlock = {
+                "Index": index,
+                "Timestamp": str(datetime.now()),
+                "BPM": bpm, #instead of transactions
+                "PrevHash": prevHash,
+                "Validator": address
+            }
+            newBlock["Hash"] = self.hashing(newBlock)
+            assert self.is_block_valid(newBlock)
+            self.myCurrBlock = newBlock
+            return newBlock
 
     def get_blocks_from_nodes(self):
         if self.nodes:
@@ -93,11 +134,10 @@ class Blockchain(object):
                 #resp = requests.get('http://{}/newblock'.format(node))
                 node.add_another_block(self.myCurrBlock)
                 resp = node.generate_new_block()
-                if self.is_block_valid(resp): #resp.json()
-                    #self.tempBlocks.append(resp.json())
-                    if not resp['Validator'] in self.validators:
-                        self.tempBlocks.append(resp)
-                        self.validators.add(resp['Validator'])
+                if self.is_block_valid(resp) and resp['Validator'] in self.pool: #resp.json()
+                    self.tempBlocks.append(resp.json())
+                if not resp['Validator'] in self.validators:
+                    self.validators.add(resp['Validator'])
 
     def add_another_block(self, another_block):
         if self.is_block_valid(another_block):
@@ -106,16 +146,15 @@ class Blockchain(object):
                 self.validators.add(another_block['Validator'])
 
 
-    def pick_winner(self):
-        """Creates a lottery pool of validators and choose the validator
-            who gets to forge the next block. Random selection stakeed by amount of token staked
-            Do this every 30 seconds
+
+    def pick_proposer(self):
+        """Picks the block propser randomly among the current witness pool, weighted by their account weight
         """
         winner = []
 
         self.tempBlocks.append(self.myCurrBlock)
         self.validators.add(self.myCurrBlock['Validator'])
-        for validator in self.validators:
+        for validator in self.pool:
             acct = (validator.rsplit(sep=', '))
             acct.append(int(acct[1]) * int(acct[2]))
             if winner and acct[-1]:
@@ -124,16 +163,17 @@ class Blockchain(object):
                 winner = acct if acct[-1] else winner
         if winner:
             return winner
-        for validator in self.validators:
+        for validator in self.pool:
             acct = (validator.rsplit(sep=', '))
             acct.append((int(acct[1]) + int(acct[2]))/len(acct[0]))
             if winner:
                 winner = acct if winner[-1] < acct[-1] else winner
             else:
                 winner = acct
+            validator['Votes For'] = 0
         return winner
 
-    def pos(self):
+    def dpos(self):
         """
         #get other's stakes
         #add owns claim
@@ -143,15 +183,15 @@ class Blockchain(object):
         print(str(self.myAccount) + ' =======================> Getting Valid chain\n')
         self.resolve_conflict()
         time.sleep(1)
-        self._pos()
-        print('***Calling other nodes to announce theirs***' + "\n")
+        self._dpos()
+        print('***Running Voting Process***' + "\n")
         time.sleep(1)
         for node in self.nodes:
-            node._pos()
+            node._dpos()
         time.sleep(1)
         for block in self.tempBlocks:
             validator = block['Validator'].rsplit(', ')
-            if validator[0] == self.pick_winner()[0]:
+            if validator[0] == self.pick_proposer()[0]:
                 new_block = block
                 break
             else:
@@ -174,19 +214,25 @@ class Blockchain(object):
             if self.myAccount['Address'] != acct[0]:
                 self.myAccount['Age'] += 1
             else:
-                self.myAccount['stake'] += (randint(1, 10) * self.myAccount['Age'])
+                self.myAccount['Weight'] += (randint(1, 10) * self.myAccount['Age'])
                 self.myAccount['Age'] = 0
+                self.myAccount['Votes For'] = 0
         self.tempBlocks = []
         self.myCurrBlock = {}
-        self.validators = set()
+        self.pool = set()
 
 
 
-    def _pos(self):
+    def _dpos(self):
         print("Coming from ==========================> " + str(self.myAccount) + "\n")
         time.sleep(1)
+        print('***Running Voting Process***' + "/n")
+        time.sleep(2)
+        length = len(self.validators)
+        number = ranndom(0,length)
+        self.voting(self,self.validators[number][0])
         print('***Generating new stake block***' + "\n")
-        time.sleep(1)
+        time.sleep(2)
         self.generate_new_block()
         print('***Exchanging temporary blocks with other nodes***' + "\n")
         time.sleep(1)
@@ -222,12 +268,13 @@ class Blockchain(object):
     def add_another_node(self, another_node):
         self.nodes.add(another_node)
 
+
     @staticmethod
     def get_validator(address):
-        return ', '.join([address['Address'], str(address['stake']), str(address['Age'])])
+        return ', '.join([address['Address'], str(address['Weight']), str(address['Age'])])
 
     def generate_genesis_block(self, genesisblock):
-        address = {'Address': 'eltneg', 'stake': 50, 'Age': 0}
+        address = {'Address': 'eltneg', 'Weight': 50, 'Age': 0}
         address = self.get_validator(address)
         genesisblock['Index'] = 0 if not genesisblock['Index'] else genesisblock['Index']
         genesisblock['Timestamp'] = str(datetime.now()) if not genesisblock['Timestamp'] else genesisblock['Timestamp']
@@ -239,10 +286,16 @@ class Blockchain(object):
 
 def main():
     """Run test"""
-    account = {'Address': 'eltneg', 'stake': 50}
-    account2 = {'Address': 'account2', 'stake': 55}
-    account3 = {'Address': 'account3', 'stake': 43}
-    account4 = {'Address': 'account4', 'stake': 16}
+    account = {'Address': 'eltneg', 'Weight': 50}
+    account2 = {'Address': 'account2', 'Weight': 55}
+    account3 = {'Address': 'account3', 'Weight': 43}
+    account4 = {'Address': 'account4', 'Weight': 16}
+    n_account = {'Address': 'Account 1', 'Weight': 11}
+    m_account = {'Address': 'Account 2', 'Weight': 33}
+    i_account = {'Address': 'Account 3', 'Weight': 69}
+    v_account = {'Address': 'Account 4', 'Weight': 42}
+    k_account = {'Address': 'Account 5', 'Weight': 23}
+
     blockchain = Blockchain(GENESIS_BLOCK, account)
     blockchain.generate_new_block(52)
 
@@ -260,23 +313,16 @@ def main():
     blockchain.get_blocks_from_nodes()
     blockchain2.get_blocks_from_nodes()
 
-    blockchain.pick_winner()
+    blockchain.pick_proposer()
     #check if temp blocks are same
 
-    blockchain.pos()
-    blockchain2.pos()
-    blockchain3.pos()
-
-    blockchain4 = Blockchain(GENESIS_BLOCK4, account4)
-    blockchain4.add_new_node(blockchain)
-    blockchain4.add_new_node(blockchain2)
-    blockchain4.add_new_node(blockchain3)
-    blockchain4.pos()
-    clients.append(blockchain4)
+    blockchain.dpos()
+    blockchain2.dpos()
+    blockchain3.dpos()
     while True:
         print('============================================ \n\n')
-        client = clients[randint(0, 3)]
-        client.pos()
+        client = clients[randint(0, 2)]
+        client.dpos()
 
 if __name__ == '__main__':
     main()
