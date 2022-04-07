85c2711863294ab1a30f2101faca1be79312d9fb
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-04-06 20:03:43 -0400

    Update blockchainutils.py

--- src/adamnite/blockchainutils.py
+++ src/adamnite/blockchainutils.py
@@ -10,6 +10,9 @@ class Blockchain_Util:
         hash_object = hashlib.sha512(item.encode())
         item = hash_object.hexdigest()
         return item
+    def hash_sha512(of: bytes):
+        assert isinstance(of, bytes)
+        return bytes(hashlib.sha512(of).digest())
 
     @staticmethod
     def encode(object):
