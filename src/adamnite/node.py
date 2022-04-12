f1be25bbaf217cd0b5be8be3871da26e4baa2c0d
Marcos Gamez <dev@marcos-gamez.ch>
2022-04-12 11:03:35 +0200

    node.py ping pong server.

--- src/adamnite/node.py
+++ src/adamnite/node.py
@@ -2,71 +2,62 @@ import asyncio
 import time
 
 from socket import socket, AF_INET6, SOCK_STREAM
+from adamnite.serialization import INT_SIZE, from_number
+
+TIMEOUT = 0.3
 
 
 class Peer:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, reader, writer):
         self.ip = ip
         self.port = port
+        self.reader: Reader = reader
+        self.writer: Writer = writer
         self.last_seen = int(time.time())
+        self.connected = True
+        self.loop = asyncio.get_event_loop()
+        self.loop.create_task(self.incoming())
 
-    def dictionary(self) -> dict:
-        address = f'{self.ip}:{self.port}'
-        return {address: Peer(self.ip, self.port)}
+    async def incoming(self):
+        size, _ = from_number(await self.reader.read(INT_SIZE), int())
+        message = await asyncio.shield(
+            asyncio.wait_for(
+                self.reader.read(size),
+                timeout=TIMEOUT
+            )
+        )
+        if message == b'PING':
+            self.writer.write(b'PONG')
+
+    # __hash__ and __eq__ make Peer comparable to each other by ip
+    def __hash__(self):
+        return hash(self.ip)
+
+    def __eq__(self, other):
+        assert isinstance(other, type(self)), NotImplemented
+        return self.ip == other.ip
 
 
 class Node:
     def __init__(self, port=6101):
-        self.peers: dict = Peer('127.0.0.1', 6199).dictionary()
+        self.peers: set = set()
         self.sock = socket(AF_INET6, SOCK_STREAM)
         self.sock.bind(('::', port))
 
-    def start(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.serve_forever())
+    async def start_serving(self):
+        server = await asyncio.start_server(self.accept, sock=self.sock)
+        await server.serve_forever()
 
-    async def serve_forever(self):
-        await asyncio.gather(
-            self.server(),
-            self.client(),
-        )
-
-    async def server(self):
-        server = await asyncio.start_server(self.receive, sock=self.sock)
-        async with server:
-            await server.serve_forever()
-
-    async def client(self):
-        while True:
-            await self.connect()
-
-    async def receive(self, reader, writer):
+    async def accept(self, reader, writer):
         ip, port, _, _ = writer.get_extra_info('peername')
-        self.peers.update(Peer(ip, port).dictionary())
-        await receive(reader, writer)
-
-    async def connect(self):
-        for address, p in self.peers.items():
-            conn = asyncio.open_connection(p.ip, p.port)
-            try:
-                reader, writer = await asyncio.wait_for(conn, timeout=3)
-            except ConnectionError:
-                continue
-            await send(writer)
-        await asyncio.sleep(1)
+        self.peers.add(Peer(ip, port, reader, writer))
 
 
-async def receive(reader, writer):
-    request: bytes = b'Hi i am the Server\n'
-    writer.write(request)
-    request: bytes = await reader.read(255)
-    writer.write(request + b'\n')
-    writer.close()
+class Reader:
+    async def read(self, size: int) -> bytes:
+        pass
 
 
-async def send(writer):
-    writer.write(b'Hi i am the Client\n')
-    writer.close()
-
-
-Node().start()
+class Writer:
+    def write(self, msg: bytes):
+        pass
---
+++ src/tests/test_node.py
@@ -0,0 +1,45 @@
+import asyncio
+import random
+import unittest
+
+from adamnite.node import Node, TIMEOUT
+from adamnite.serialization import BYTE_ORDER, INT_SIZE
+
+
+PORT = random.randint(6101, 6198)
+
+
+class TestNode(unittest.TestCase):
+    node = Node(port=PORT)
+    loop = asyncio.get_event_loop()
+    loop.create_task(node.start_serving())
+
+    def test_server_serving(self):
+        async def run_test(port=PORT):
+            try:
+                await connect(port)
+            except ConnectionError:
+                return False
+            return True
+        self.assertTrue(self.loop.run_until_complete(run_test()))
+        self.assertFalse(self.loop.run_until_complete(run_test(6199)))
+        self.assertTrue(0 < len(self.node.peers))
+
+    def test_peer_responding(self):
+        async def run_test():
+            reader, writer = await connect(PORT)
+            size = 4
+            writer.write(size.to_bytes(INT_SIZE, BYTE_ORDER))
+            writer.write(b'PING')
+            message = await asyncio.wait_for(reader.read(4), timeout=TIMEOUT)
+            return message
+        self.assertEqual(self.loop.run_until_complete(run_test()), b'PONG')
+
+
+async def connect(port=6101):
+    conn = asyncio.open_connection("127.0.0.1", port)
+    try:
+        reader, writer = await asyncio.wait_for(conn, timeout=TIMEOUT)
+    except ConnectionError:
+        raise ConnectionError
+    return reader, writer
